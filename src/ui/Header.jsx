1113d8a42bc7d50bfe316ced220ed0e684d844d3
shrishgour <shrishgour02071992@gmail.com>
2025-07-11 13:18:48 +0530

    first commit

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast Hot Pizza Co.</Link>
+
+      <p>Hot Pizza</p>
+    </header>
+  );
+}
+
+export default Header;
