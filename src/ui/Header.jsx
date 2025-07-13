ca7dfdde2714c79ca449f050a3a4c969a3d55dbe
shrishgour <shrishgour02071992@gmail.com>
2025-07-13 17:42:38 +0530

    data setup done

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">Fast Hot Pizza Co.</Link>
+      <SearchOrder />
 
       <p>Hot Pizza</p>
     </header>
